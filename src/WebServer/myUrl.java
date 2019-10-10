7a6a8f54533bc52bed9c2841c144a97119e25675
if18b136 <if18b136@technikum-wien.at>
2019-10-10 23:25:20 +0200

    Parameter Count - bad Code, should be changed for final Project.

--- src/WebServer/myUrl.java
+++ src/WebServer/myUrl.java
@@ -47,7 +47,24 @@ public class myUrl implements Url {
 
     @Override
     public int getParameterCount() {
-        return 0;
+        try {
+            if(this.getRawUrl().split("\\?")[0] == this.rawUrl){
+                return 0;
+            }
+            String query = this.getRawUrl().split("\\?")[1];
+            Map<String,String> params = this.params;
+            for (String param : query.split("&")){
+                String[] keyValue = param.split("=", 2);
+                String key = URLDecoder.decode(keyValue[0], "UTF-8");
+                String value = keyValue.length > 1 ? URLDecoder.decode(keyValue[1], "UTF-8"): "";
+                if(!key.isEmpty()){
+                    params.put(key,value);
+                }
+            }
+            return params.size();
+        } catch(UnsupportedEncodingException ex){
+            throw new IllegalStateException(ex); // won't happen with UTF-8 encoding.
+        }
     }
 
     @Override
