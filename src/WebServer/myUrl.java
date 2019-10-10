9ef45ef024a4bd6b5431688030d23aa8b4efada6
if18b136 <if18b136@technikum-wien.at>
2019-10-10 22:57:52 +0200

    Added Parameter Dictionary - only one value per key possible.

--- src/WebServer/myUrl.java
+++ src/WebServer/myUrl.java
@@ -1,11 +1,15 @@
 package WebServer;
 
 import BIF.SWE1.interfaces.Url;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class myUrl implements Url {
 
     private String rawUrl;
+    private Map<String, String> params = new LinkedHashMap<>();
 
     public void setUrl(String path){
         this.rawUrl = path;
@@ -24,7 +28,21 @@ public class myUrl implements Url {
 
     @Override
     public Map<String, String> getParameter() {
-        return null;
+        try {
+            String query = this.getRawUrl().split("\\?")[1];
+            Map<String,String> params = this.params;
+            for (String param : query.split("&")){
+                String[] keyValue = param.split("=", 2);
+                String key = URLDecoder.decode(keyValue[0], "UTF-8");
+                String value = keyValue.length > 1 ? URLDecoder.decode(keyValue[1], "UTF-8"): "";
+                if(!key.isEmpty()){
+                    params.put(key,value);
+                }
+            }
+            return params;
+        } catch(UnsupportedEncodingException ex){
+            throw new IllegalStateException(ex); // won't happen with UTF-8 encoding.
+        }
     }
 
     @Override
